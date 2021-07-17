8477b19be023e206bf646224db7f78536f3cdb28
Ruimas-UTI <allpraisethelord@gmail.com>
2021-07-17 23:54:51 +0600

    initial data scraped

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 const puppeteer = require('puppeteer')
-const $ = require('cheerio')
+const cheerio = require('cheerio')
 const CronJob = require('cron').CronJob;
 const nodeMailer = require('nodemailer');
 
+const url = 'https://www.amazon.com/PlayStation-DualSense-Wireless-Controller-Midnight-5/dp/B094WL86N5/ref=sr_1_1?dchild=1&keywords=ps5&qid=1626543209&sr=8-1'
+//Instansiate the url
+async function browserConfig(){
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage();
+    await page.goto(url);
+    return page
+}
+// Price check function
+async function priceCheck(page){
+    await page.reload;
+    let markup = await page.evaluate(() => document.body.innerHTML);
+    const $ = cheerio.load(markup)
+}
+//Monitoring function
+async function monitor(){
+    let page = await browserConfig();
+    await priceCheck(page)
+}
+monitor();
